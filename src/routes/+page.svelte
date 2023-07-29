26060b39ad28d85a0f12947b1068e6d29a690465
tokizuoh <tokizuoh@gmail.com>
2023-07-29 17:26:42 +0900

    add links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,9 +16,10 @@
 
 <h2>Other Links</h2>
 <ul>
-    <li>GitHub</li>
-    <li>Speacker Deck</li>
-    <li>Scrapbox</li>
+    <li><a href="https://github.com/tokizuoh">GitHub</a></li>
+    <li><a href="https://tokizuoh.hatenablog.com/">Blog</a></li>
+    <li><a href="https://speakerdeck.com/tokizuoh">Speacker Deck</a></li>
+    <li><a href="https://scrapbox.io/tokizuoh-public/">Scrapbox</a></li>
 </ul>
 
 <style>
