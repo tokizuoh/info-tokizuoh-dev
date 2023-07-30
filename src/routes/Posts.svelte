9d5e21c620c4f0c710801a3456ee035a60cd2987
tokizuoh <tokizuoh@gmail.com>
2023-07-30 14:38:23 +0900

    add fetch processing recently posts

--- src/routes/Posts.svelte
+++ src/routes/Posts.svelte
@@ -1,24 +1,37 @@
+<script>
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        fetchItems();
+    });
+
+    async function fetchItems() {
+        const parser = new DOMParser();
+        const res = await fetch("https://tokizuoh.hatenablog.com/rss?size=5");
+        const data = await res.text();
+        const obj = await parser.parseFromString(data, "text/xml");
+        const items = obj.querySelectorAll("item");
+        const links = await Promise.all(Array.from(items).map(async (item) => {
+            const itemDom = parser.parseFromString(item.innerHTML, "text/html");
+            const title = itemDom.querySelector("title").textContent;
+            const url = itemDom.querySelector("body").firstChild.nodeValue;
+            return [title, url];
+        }));
+        return links;
+    }
+</script>
+
 <h2>Posts</h2>
-<ul>
-    <li>
-        <a href="https://tokizuoh.hatenablog.com/entry/2023/07/17/174904"
-        target="_blank"
-        rel="noopener noreferrer">
-            ダジャレで戦うDMM英会話
-        </a>
-    </li>
-    <li>
-        <a href="https://tokizuoh.hatenablog.com/entry/2023/07/09/172049"
-        target="_blank"
-        rel="noopener noreferrer">
-            RustでScrapboxのハッシュタグに紐づくページ数を集計するツールを作った
-        </a>
-    </li>
-    <li>
-        <a href="https://tokizuoh.hatenablog.com/entry/2023/07/09/172049"
-        target="_blank"
-        rel="noopener noreferrer">
-            2023年6月を振り返る
-        </a>
-    </li>
-</ul>
+{#await fetchItems()}
+    <p>ロード中</p>
+{:then links}
+    <ul>
+        {#each links as l}
+            <li>
+                <a href="{l[1]}" target="_blank" rel="noopener noreferrer">
+                    {l[0]}
+                </a>
+            </li>
+        {/each}
+    </ul>
+{/await}
