cc33de99b011e4477a755f1f91e20975cef08498
tokizuoh <tokizuoh@gmail.com>
2023-08-12 16:11:23 +0900

    add gitub activity link
    ref: https://twitter.com/onk/status/1427967332731686924

--- src/routes/Links.svelte
+++ src/routes/Links.svelte
@@ -1,6 +1,15 @@
+<script>
+    const github_activity_url = "https://github.com/pulls?q=involves%3Atokizuoh+-user%3Atokizuoh"
+</script>
+
 <h2>Links</h2>
 <ul>
-    <li><a href="https://github.com/tokizuoh">GitHub</a></li>
+    <li>
+        <a href="https://github.com/tokizuoh">GitHub</a>
+        <ul>
+            <li><a href={github_activity_url}>{github_activity_url}</a></li>
+        </ul>
+    </li>
     <li><a href="https://tokizuoh.hatenablog.com/">Blog</a></li>
     <li><a href="https://speakerdeck.com/tokizuoh">Speacker Deck</a></li>
     <li><a href="https://scrapbox.io/tokizuoh-public/">Scrapbox</a></li>
