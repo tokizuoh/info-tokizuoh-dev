e07dd0404d08cbc0b5c106de494c50823ed48895
tokizuoh <tokizuoh@gmail.com>
2023-08-26 17:53:58 +0900

    add rotate icon when click icon

--- src/routes/Icon.svelte
+++ src/routes/Icon.svelte
@@ -1,13 +1,45 @@
 <script>
     import { base } from '$app/paths';
+
+    let isRotated = false;
+    let rotationComplete = true;
+
+    async function toggleRotation() {
+        if (rotationComplete) {
+            rotationComplete = false;
+            isRotated = !isRotated;
+
+            const image = document.querySelector('.rotate-icon');
+            image.classList.toggle('rotate');
+
+            await image.animate(
+                [
+                    { transform: 'rotate(0deg)' },
+                    { transform: 'rotate(360deg)' }
+                ],
+                {
+                    duration: 1000,
+                    easing: 'ease-out',
+                    fill: 'forwards'
+                }
+            ).finished;
+
+            isRotated = !isRotated;
+            rotationComplete = true;
+        }
+    }
 </script>
 
 <p>
-    <img id="icon" src={`${base}/icon.png`} alt="Deformed penguin illustration">
+    <img class="rotate-icon {isRotated ? 'rotate' : ''}"
+         src={`${base}/icon.png`}
+         alt="Deformed penguin illustration"
+         on:click={toggleRotation}
+    >
 </p>
 
 <style>
-#icon {
+.rotate-icon {
     border-radius:50%;
     width: 75px;
     height: 75px;
