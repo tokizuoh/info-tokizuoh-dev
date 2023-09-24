2e452e637a2cee46abc51eba380408dd9a9f347a
tokizuoh <tokizuoh@gmail.com>
2023-09-24 14:06:24 +0900

    add year

--- src/routes/Works.svelte
+++ src/routes/Works.svelte
@@ -1,13 +1,13 @@
 <h2>Works</h2>
 <ul>
     <li>
-        <a href="https://github.com/tokizuoh/swift-9cc">tokizuoh/swift-9cc</a>
+        2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
         <ul>
             <li>C compiler written by Swift</li>
         </ul>
     </li>
     <li>
-        <a href="https://github.com/tokizuoh/faaaar">tokizuoh/faaaar</a>
+        2022 - <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
         <ul>
             <li>GraphQL Server and Playground</li>
         </ul>
