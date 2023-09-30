4783cdd46ec138e05a3f03e8fce7690d52e7ac61
tokizuoh <tokizuoh@gmail.com>
2023-09-30 15:28:06 +0900

    add: monthly-distance

--- src/routes/Works.svelte
+++ src/routes/Works.svelte
@@ -1,6 +1,12 @@
 <h2>Works</h2>
 <ul>
     <li>
+        2023 - <a href="https://tokizuoh.hatenablog.com/monthly-distance">今月のキョリ</a>
+        <ul>
+            <li>iOS App for displays the total distance exercised</li>
+        </ul>
+    </li>
+    <li>
         2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
         <ul>
             <li>C compiler written by Swift</li>
