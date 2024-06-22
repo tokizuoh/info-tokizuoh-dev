4c931563e6f5a63cef3204d9779bf0a956b08606
tokizuoh <tokizuoh@gmail.com>
2024-06-22 11:52:21 +0900

    feat: translate highlights's descriptions

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -1,21 +1,21 @@
 <h2>Highlights</h2>
 <ul>
 	<li>
-		2023 - <a href="https://tokizuoh.hatenablog.com/monthly-distance">今月のキョリ</a>
+		2023 - <a href="https://tokizuoh.hatenablog.com/monthly-distance">monthly-distance</a>
 		<ul>
-			<li>運動で移動した合計距離を表示するiOSアプリ</li>
+			<li>iOS app that displays the total distance traveled through exercise</li>
 		</ul>
 	</li>
 	<li>
 		2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
 		<ul>
-			<li>Swiftで書いたCコンパイラ</li>
+			<li>C compiler written in swift</li>
 		</ul>
 	</li>
 	<li>
 		2022 - <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
 		<ul>
-			<li>GraphQLサーバーとクエリを実行できるプレイグラウンド</li>
+			<li>GraphQL server with an interactive playground for executing queries</li>
 		</ul>
 	</li>
 </ul>
