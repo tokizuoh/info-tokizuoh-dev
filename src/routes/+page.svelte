a5216023f92de54ade1080de95c68df3fcb79ef9
tokizuoh <tokizuoh@gmail.com>
2024-06-22 11:45:57 +0900

    rename: Works -> Highlights

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-    import Icon from "./Icon.svelte"
-    import Posts from "./Posts.svelte"
-    import Links from "./Links.svelte"
-    import Works from "./Works.svelte"
-    import Copyright from "./Copyright.svelte"
-    
-    const title = "tokizuoh"
-    const description = `
+	import Icon from './Icon.svelte';
+	import Posts from './Posts.svelte';
+	import Links from './Links.svelte';
+	import Highlights from './Highlights.svelte';
+	import Copyright from './Copyright.svelte';
+
+	const title = 'tokizuoh';
+	const description = `
         I'm working as a software engineer in Japan.
-    `
+    `;
 </script>
 
 <title>{title}</title>
@@ -17,50 +17,50 @@
 
 <Posts />
 <Links />
-<Works />
+<Highlights />
 <Copyright />
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
 
-:global(body) {
-    font-family: 'Noto Sans JP', sans-serif;
+	:global(body) {
+		font-family: 'Noto Sans JP', sans-serif;
 
-    background-color: rgb(25, 28, 35); 
-    color: #EEEEEE;
+		background-color: rgb(25, 28, 35);
+		color: #eeeeee;
 
-    width: 800px;
-    margin-top: 80px;
-    margin-right: auto;
-    margin-left: auto;
-    margin-bottom: 100px;
+		width: 800px;
+		margin-top: 80px;
+		margin-right: auto;
+		margin-left: auto;
+		margin-bottom: 100px;
 
-    @media screen and (max-width: 959px) {
-        margin-left: 24px;
-        margin-right: 24px;
-        width: auto;
-    }
-}
+		@media screen and (max-width: 959px) {
+			margin-left: 24px;
+			margin-right: 24px;
+			width: auto;
+		}
+	}
 
-:global(h2) {
-    margin-top: 60px;
-    margin-bottom: 0px;
-}
+	:global(h2) {
+		margin-top: 60px;
+		margin-bottom: 0px;
+	}
 
-:global(li) {
-    margin-bottom: 2px;
-    color: #BBBBBB;
-}
+	:global(li) {
+		margin-bottom: 2px;
+		color: #bbbbbb;
+	}
 
-:global(a) {
-    color: #BBBBBB;
-    text-decoration:underline;
-}
+	:global(a) {
+		color: #bbbbbb;
+		text-decoration: underline;
+	}
 
-:global(a:hover) {
-    color: white;
-}
-p {
-    color: #BBBBBB;
-}
+	:global(a:hover) {
+		color: white;
+	}
+	p {
+		color: #bbbbbb;
+	}
 </style>
---
+++ src/routes/Highlights.svelte
@@ -0,0 +1,21 @@
+<h2>Highlights</h2>
+<ul>
+	<li>
+		2023 - <a href="https://tokizuoh.hatenablog.com/monthly-distance">今月のキョリ</a>
+		<ul>
+			<li>運動で移動した合計距離を表示するiOSアプリ</li>
+		</ul>
+	</li>
+	<li>
+		2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
+		<ul>
+			<li>Swiftで書いたCコンパイラ</li>
+		</ul>
+	</li>
+	<li>
+		2022 - <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
+		<ul>
+			<li>GraphQLサーバーとクエリを実行できるプレイグラウンド</li>
+		</ul>
+	</li>
+</ul>
