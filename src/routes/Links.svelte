be41fc64cdfd9a099f729fb24e2d7aa4db4c47b4
tokizuoh <tokizuoh@gmail.com>
2024-07-13 23:44:10 +0900

    format

--- src/routes/Links.svelte
+++ src/routes/Links.svelte
@@ -1,16 +1,17 @@
 <script>
-    const github_activity_url = "https://github.com/pulls?q=involves%3Atokizuoh+-user%3Atokizuoh++is%3Apublic"
+	const github_activity_url =
+		'https://github.com/pulls?q=involves%3Atokizuoh+-user%3Atokizuoh++is%3Apublic';
 </script>
 
 <h2>Links</h2>
 <ul>
-    <li>
-        <a href="https://github.com/tokizuoh">GitHub</a>
-        <ul>
-            <li><a href={github_activity_url}>{github_activity_url}</a></li>
-        </ul>
-    </li>
-    <li><a href="https://tokizuoh.hatenablog.com/">Blog</a></li>
-    <li><a href="https://zenn.dev/tokizuoh">Zenn</a></li>
-    <li><a href="https://speakerdeck.com/tokizuoh">Speacker Deck</a></li>
+	<li>
+		<a href="https://github.com/tokizuoh">GitHub</a>
+		<ul>
+			<li><a href={github_activity_url}>{github_activity_url}</a></li>
+		</ul>
+	</li>
+	<li><a href="https://tokizuoh.hatenablog.com/">Blog</a></li>
+	<li><a href="https://zenn.dev/tokizuoh">Zenn</a></li>
+	<li><a href="https://speakerdeck.com/tokizuoh">Speacker Deck</a></li>
 </ul>
