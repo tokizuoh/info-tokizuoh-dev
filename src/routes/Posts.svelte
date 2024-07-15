146fecca0ff89e23d8a3b5c9b5cf326ae3593cd2
tokizuoh <tokizuoh@gmail.com>
2024-07-15 14:54:12 +0900

    feat: read json from local

--- src/routes/Posts.svelte
+++ src/routes/Posts.svelte
@@ -1,44 +1,20 @@
 <script context="module">
-	async function load() {
-		const cacheLife = 3600;
+	import latest_posts from './../data/latest_posts.json';
 
-		var cachedData = localStorage.getItem('posts');
-		if (cachedData) {
-			cachedData = JSON.parse(cachedData);
-			var expired = parseInt(Date.now() / 1000) - cachedData.cachedTime > cacheLife;
-		}
-
-		if (cachedData && !expired) {
-			return cachedData.links;
-		} else {
-			const parser = new DOMParser();
-			const res = await fetch('https://tokizuoh.hatenablog.com/rss?size=5');
-			const data = await res.text();
-			const obj = await parser.parseFromString(data, 'text/xml');
-			const items = obj.querySelectorAll('item');
-			const links = await Promise.all(
-				Array.from(items).map(async (item) => {
-					const itemDom = parser.parseFromString(item.innerHTML, 'text/html');
-					const title = itemDom.querySelector('title').textContent;
-					const url = itemDom.querySelector('body').firstChild.nodeValue;
-					const pubDate = itemDom.querySelector('pubdate').firstChild.nodeValue;
-					const pubDateStr = new Date(pubDate).toLocaleDateString('ja-JP', {
-						year: 'numeric',
-						month: '2-digit',
-						day: '2-digit'
-					});
-					return [title, url, pubDateStr];
-				})
-			);
-			var cache = { links: links, cachedTime: parseInt(Date.now() / 1000) };
-			localStorage.setItem('posts', JSON.stringify(cache));
-			return links;
-		}
-	}
+	const links = latest_posts.map((item) => {
+		const title = item.title;
+		const url = item.url;
+		const pubDateStr = new Date(item.publishedAt).toLocaleDateString('ja-JP', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit'
+		});
+		return [title, url, pubDateStr];
+	});
 </script>
 
 <h2>Posts</h2>
-{#await load() then links}
+{#if links}
 	<ul>
 		{#each links as l}
 			<li>
@@ -51,7 +27,9 @@
 			</li>
 		{/each}
 	</ul>
-{/await}
+{:else}
+	<p>Loading...</p>
+{/if}
 
 <style>
 	li div {
