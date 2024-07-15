865a2fe27136ca3ef173f0a5702fa862d570bf6e
tokizuoh <tokizuoh@gmail.com>
2024-07-15 16:21:14 +0900

    fix

--- src/routes/Posts.svelte
+++ src/routes/Posts.svelte
@@ -1,28 +1,42 @@
-<script context="module">
-	import latest_posts from './../data/latest_posts.json';
+<script>
+	import latest_posts from '../data/latest_posts.json';
 
 	const links = latest_posts.map((item) => {
 		const title = item.title;
 		const url = item.url;
+		const source = item.source;
 		const pubDateStr = new Date(item.publishedAt).toLocaleDateString('ja-JP', {
 			year: 'numeric',
 			month: '2-digit',
 			day: '2-digit'
 		});
-		return [title, url, pubDateStr];
+
+		let sourceDisplay;
+		switch (source) {
+			case 'zenn':
+				sourceDisplay = 'Zenn';
+				break;
+			case 'hatena':
+				sourceDisplay = 'Hatena Blog';
+				break;
+			default:
+				sourceDisplay = 'Unknown';
+				break;
+		}
+
+		return { title, url, source, sourceDisplay, pubDateStr };
 	});
 </script>
 
 <h2>Posts</h2>
-{#if links}
+{#if links.length > 0}
 	<ul>
-		{#each links as l}
-			<li>
-				<div>
-					<a href={l[1]}>
-						{l[0]}
-					</a>
-					<div class="pub-date">({l[2]})</div>
+		{#each links as link}
+			<li class="post-item">
+				<a href={link.url} class="title">{link.title}</a>
+				<div class="meta">
+					<span class="source-badge {link.source}">{link.sourceDisplay}</span>
+					<span class="pub-date">{link.pubDateStr}</span>
 				</div>
 			</li>
 		{/each}
@@ -32,13 +46,14 @@
 {/if}
 
 <style>
-	li div {
+	.meta {
 		display: inline;
+		font-size: 0.85em;
+		color: #888888;
+		margin-left: 8px;
 	}
 
 	.pub-date {
-		color: #888888;
 		margin-left: 4px;
-		font-size: 95%;
 	}
 </style>
