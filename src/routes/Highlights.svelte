3df7c6c97c240950f4e2721be69a8ab02cab128e
tokizuoh <tokizuoh@gmail.com>
2024-09-10 12:07:48 +0900

    update: japanese

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -1,21 +1,23 @@
 <h2>Highlights</h2>
 <ul>
 	<li>
-		2024 - <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz">Swift Concurrency Quiz: Avoiding Main Thread Blocking</a>
+		2024 - <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
+			>Swift Concurrency Quiz: Avoiding Main Thread Blocking</a
+		>
 		<ul>
-			<li>Quiz on preventing unintentional main-thread blocking with swift concurrency</li>
+			<li>iOSDC Japan 2024にて登壇したSwift Concurrencyクイズのスライド</li>
 		</ul>
 	</li>
 	<li>
 		2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
 		<ul>
-			<li>C compiler written in swift</li>
+			<li>Swiftで書いたCコンパイラ</li>
 		</ul>
 	</li>
 	<li>
 		2022 - <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
 		<ul>
-			<li>GraphQL server with an interactive playground for executing queries</li>
+			<li>GraphQLサーバーとクエリを実行する自作プレイグラウンド</li>
 		</ul>
 	</li>
 </ul>
