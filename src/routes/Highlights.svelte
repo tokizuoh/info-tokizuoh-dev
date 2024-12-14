e28bd63fb8e2df85afc46010344a0d7058922713
tokizuoh <tokizuoh@gmail.com>
2024-12-14 16:01:07 +0900

    remove

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -1,7 +1,7 @@
 <h2>Highlights</h2>
 <ul>
 	<li>
-		2024 - <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
+		2024 <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
 			>メインスレッドをブロックさせないためのSwift Concurrencyクイズ</a
 		>
 		<ul>
@@ -9,13 +9,13 @@
 		</ul>
 	</li>
 	<li>
-		2023 - <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
+		2023 <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
 		<ul>
 			<li>Swiftで書いたCコンパイラ</li>
 		</ul>
 	</li>
 	<li>
-		2022 - <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
+		2022 <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
 		<ul>
 			<li>GraphQLサーバーとクエリを実行する自作プレイグラウンド</li>
 		</ul>
