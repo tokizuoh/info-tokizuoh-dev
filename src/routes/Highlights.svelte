afedc3efe680695e651510afb58f0c0b7080f64c
tokizuoh <tokizuoh@gmail.com>
2025-08-09 11:50:52 +0900

    add: highlights 2025

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -1,6 +1,14 @@
 <h2>Highlights</h2>
 <ul>
 	<li>
+		2025 <a href="https://developer.hatenastaff.com/entry/inside-gigaviewer-for-apps-13"
+			>大規模トラフィックを支えるGigaViewer for AppsのAPI負荷対策</a
+		>
+		<ul>
+			<li>所属チームでの負荷対策の取り組みをまとめた技術記事</li>
+		</ul>
+	</li>
+	<li>
 		2024 <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
 			>メインスレッドをブロックさせないためのSwift Concurrencyクイズ</a
 		>
