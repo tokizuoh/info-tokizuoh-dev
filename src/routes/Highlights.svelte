00f9065c572fc1f858890b12dcfb039835ce9246
tokizuoh <tokizuoh@gmail.com>
2025-08-09 12:01:05 +0900

    chore

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -17,13 +17,13 @@
 		</ul>
 	</li>
 	<li>
-		2023 <a href="https://github.com/tokizuoh/swift-9cc">swift-9cc</a>
+		2023 <a href="https://github.com/tokizuoh/swift-9cc">tokizuoh/swift-9cc</a>
 		<ul>
 			<li>Swiftで書いたCコンパイラ</li>
 		</ul>
 	</li>
 	<li>
-		2022 <a href="https://github.com/tokizuoh/faaaar">faaaar</a>
+		2022 <a href="https://github.com/tokizuoh/faaaar">tokizuoh/faaaar</a>
 		<ul>
 			<li>GraphQLサーバーとクエリを実行する自作プレイグラウンド</li>
 		</ul>
