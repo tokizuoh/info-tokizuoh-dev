7624a2a6179db91adecfdacdb84b301525a9957c
tokizuoh <tokizuoh@gmail.com>
2025-08-09 22:49:12 +0900

    add: Findy Toolsの記事

--- src/routes/Highlights.svelte
+++ src/routes/Highlights.svelte
@@ -1,31 +1,59 @@
 <h2>Highlights</h2>
 <ul>
 	<li>
-		2025 <a href="https://developer.hatenastaff.com/entry/inside-gigaviewer-for-apps-13"
-			>大規模トラフィックを支えるGigaViewer for AppsのAPI負荷対策</a
-		>
+		2025
 		<ul>
-			<li>Hatena Developer Blogにて所属チームでの負荷対策の取り組みをまとめた記事</li>
+			<li>
+				<a href="https://developer.hatenastaff.com/entry/inside-gigaviewer-for-apps-13"
+					>大規模トラフィックを支えるGigaViewer for AppsのAPI負荷対策</a
+				>
+				<ul>
+					<li>Hatena Developer Blogにて所属チームでの負荷対策の取り組みをまとめた記事</li>
+				</ul>
+			</li>
 		</ul>
 	</li>
 	<li>
-		2024 <a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
-			>メインスレッドをブロックさせないためのSwift Concurrencyクイズ</a
-		>
+		2024
 		<ul>
-			<li>iOSDC Japan 2024にて登壇したSwift Concurrencyクイズのスライド</li>
+			<li>
+				<a href="https://speakerdeck.com/tokizuoh/swift-cocurrency-quiz"
+					>メインスレッドをブロックさせないためのSwift Concurrencyクイズ</a
+				>
+				<ul>
+					<li>iOSDC Japan 2024にて登壇したSwift Concurrencyクイズのスライド</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://findy-tools.io/products/circleci/351/176"
+					>モバイルアプリ開発におけるCircleCIの導入事例</a
+				>
+				<ul>
+					<li>Findy Toolsに寄稿した所属チームのCircleCI導入事例記事</li>
+				</ul>
+			</li>
 		</ul>
 	</li>
 	<li>
-		2023 <a href="https://github.com/tokizuoh/swift-9cc">tokizuoh/swift-9cc</a>
+		2023
 		<ul>
-			<li>Swiftで書いたCコンパイラ</li>
+			<li>
+				<a href="https://github.com/tokizuoh/swift-9cc">tokizuoh/swift-9cc</a>
+				<ul>
+					<li>Swiftで書いたCコンパイラ</li>
+				</ul>
+			</li>
 		</ul>
 	</li>
 	<li>
-		2022 <a href="https://github.com/tokizuoh/faaaar">tokizuoh/faaaar</a>
+		2022
 		<ul>
-			<li>GraphQLサーバーとクエリを実行する自作プレイグラウンド</li>
+			<li>
+				<a href="https://github.com/tokizuoh/faaaar">tokizuoh/faaaar</a>
+				<ul>
+					<li>GraphQLサーバーとクエリを実行する自作プレイグラウンド</li>
+				</ul>
+			</li>
 		</ul>
 	</li>
 </ul>
